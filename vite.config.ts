d73c2c85fad20a2e514c971ad958e420b6435843
bornofthemind2 <ewellwear@gmail.com>
2025-10-21 08:21:59 -0400

    Add allowedHosts to Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
+       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['four20reviews.onrender.com'],
+      },
       },
       plugins: [react()],
       define: {
